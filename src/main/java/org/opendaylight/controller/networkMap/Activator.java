85a12247c4d42182aadf74bac11575ae1d09a626
Chris Jackson <cscrj@bris.ac.uk>
2014-06-09 15:01:01 +0100

    Added all files

---
+++ src/main/java/org/opendaylight/controller/networkMap/Activator.java
@@ -0,0 +1,66 @@
+package org.opendaylight.controller.networkMap;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.apache.felix.dm.Component;
+import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
+import org.opendaylight.controller.sal.core.IContainer;
+import org.opendaylight.controller.sal.packet.IDataPacketService;
+//import org.opendaylight.controller.protocol_plugin.openflow.ITopologyServiceShimListener;
+//import org.opendaylight.controller.protocol_plugin.openflow.core.ISwitchStateListener;
+import org.opendaylight.controller.switchmanager.IInventoryListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator extends ComponentActivatorAbstractBase {
+
+    private static final Logger log = LoggerFactory.getLogger(NetworkMap.class);
+
+    @Override
+    public Object[] getImplementations() {
+        log.trace("Getting Implementations");
+
+        Object[] res = { NetworkMap.class };
+        return res;
+    }
+
+    @Override
+    public void configureInstance(Component c, Object imp, String containerName) {
+        log.trace("Configuring NetworkMap instance");
+
+        if (imp.equals(NetworkMap.class)) {
+            // Define exported and used services for NetworkMap component
+
+            Dictionary<String, Object> inventoryServiceProperties = new Hashtable<String, Object>();
+            inventoryServiceProperties.put("salListenerName",
+                    "myNetworkListener"); // give our topologyShimSistener a
+                                          // name using salListenerName
+                                          // property
+
+            c.setInterface(new String[] { IInventoryListener.class.getName() },
+                    inventoryServiceProperties);
+
+            /*
+             * Below is from example to export callback methods. We have no
+             * callbacks, as we're only listening, (for now) so do we don't need
+             * this
+             */
+
+            // Need the DataPacketService for encoding, decoding, sending data
+            // packets
+            c.add(createContainerServiceDependency(containerName)
+                    .setService(IDataPacketService.class)
+                    .setCallbacks("setDataPacketService",
+                            "unsetDataPacketService").setRequired(true));
+
+            // for accessing local container
+            c.add(createContainerServiceDependency(containerName)
+                    .setService(IContainer.class)
+                    .setCallbacks("setIContainer", "unsetIContainer")
+                    .setRequired(true));
+
+        }
+
+    }
+}
---
+++ src/main/java/org/opendaylight/controller/networkMap/NetworkMap.java
@@ -0,0 +1,152 @@
+package org.opendaylight.controller.networkMap;
+
+// OpenDaylight imports
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.opendaylight.controller.sal.core.IContainer;
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.core.Node.NodeIDType;
+import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.core.Property;
+import org.opendaylight.controller.sal.core.UpdateType;
+import org.opendaylight.controller.sal.packet.Ethernet;
+import org.opendaylight.controller.sal.packet.IDataPacketService;
+import org.opendaylight.controller.sal.packet.IListenDataPacket;
+import org.opendaylight.controller.sal.packet.IPv4;
+import org.opendaylight.controller.sal.packet.PacketResult;
+import org.opendaylight.controller.sal.packet.RawPacket;
+import org.opendaylight.controller.sal.utils.ServiceHelper;
+import org.opendaylight.controller.switchmanager.IInventoryListener;
+import org.opendaylight.controller.switchmanager.ISwitchManager;
+import org.opendaylight.controller.switchmanager.Switch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//Java imports
+/**
+ * Leverage protocols_plugins.openflow to: 1. Build a DB of all switch node
+ * properties 2. Build a topology map
+ */
+
+public class NetworkMap implements IInventoryListener, IListenDataPacket {
+
+    /*
+     * Unneeded because Node implements ToString() to create a unique identifier
+     * for each node
+     */
+    private IDataPacketService _dataPacketService; // for packet decoding and
+    // inspection
+    private IContainer _container;
+    private ISwitchManager _switchManager;
+
+    private ConcurrentHashMap<String, Node> _nodeData = new ConcurrentHashMap<String, Node>(); // properties
+    // of
+    // network
+    // nodes, we'll only keep
+    // switches
+
+    private ConcurrentMap<String, NodeConnector> _connectorData;
+    private static final Logger log = LoggerFactory.getLogger(NetworkMap.class); // log
+                                                                                 // actions
+
+    private void AccessSwitchManager() {
+        _switchManager = (ISwitchManager) ServiceHelper.getInstance(
+                ISwitchManager.class, _container.getName(), this);
+
+        List<Switch> switchUpdate = _switchManager.getNetworkDevices();
+
+    }
+
+    /* Called on update to node inventory */
+    public void notifyNode(Node node, UpdateType type,
+            Map<String, Property> propMap) {
+
+        log.trace("Received implementation agnostic node update");
+
+        if (node.getType() == NodeIDType.OPENFLOW)// we care only for openflow
+                                                  // nodes
+        {
+            String nodeId = node.toString(); // key for hashmap
+
+            switch (type) {
+            case ADDED:
+                _nodeData.put(nodeId, node);
+                // NodeDescription nodeData = readDescription(node,true);
+                break;
+            case CHANGED:
+                break;
+            case REMOVED:
+                break;
+
+            default:
+                log.trace("Uknown notifyNode update type: '" + type.toString()
+                        + "'");
+                break;
+
+            }
+
+        }
+
+        this.AccessSwitchManager();
+
+        // if nodetype = switch and openflow then stuff
+    }
+
+    /* Called on update to nodeConnector (port) inventory */
+    public void notifyNodeConnector(NodeConnector nodeConnector,
+            UpdateType type, Map<String, Property> propMap) {
+
+        log.trace("Received implementation agnostic  connector update");
+        String updateConnectorId = nodeConnector.toString();
+
+    }
+
+    @Override
+    public PacketResult receiveDataPacket(RawPacket inPkt) {
+        // TODO Auto-generated method stub
+        if (inPkt == null) {
+            return PacketResult.IGNORED;
+        }
+        log.trace("Received data packet of ", inPkt.getPacketData().length,
+                " bytes");
+
+        Ethernet decodedPkt = (Ethernet) this._dataPacketService
+                .decodeDataPacket(inPkt);
+        IPv4 ipv4Hdr = (IPv4) decodedPkt.getPayload();
+
+        byte[] payload = ipv4Hdr.getRawPayload();
+
+        return null;
+    }
+
+    /*
+     * These two methods allows to attach to the data packet service above the
+     * SAL
+     */
+    void setDataPacketService(IDataPacketService s) {
+        this._dataPacketService = s;
+    }
+
+    void unsetDataPacketService(IDataPacketService s) {
+        if (this._dataPacketService == s) {
+            this._dataPacketService = null;
+        }
+    }
+
+    /*
+     * These two methods let us attach _container to the package's container and
+     * therefore access the various managers in the controller platform
+     */
+    void setIContainer(IContainer c) {
+        _container = c;
+    }
+
+    public void unsetIContainer(IContainer s) {
+        if (this._container == s) {
+            this._container = null;
+        }
+    }
+}
